ad028da1820bc5599965fb890c2a186ed2c3f7bb
Jason Porritt <jason.porritt@atomicobject.com>
2011-06-01 23:38:31 -0400

    Removed the build directory.

--- build-cli/mcp2515.d
+++
@@ -1,5 +0,0 @@
-build-cli/mcp2515.o: mcp2515.c \
-  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/WConstants.h \
-  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/wiring.h \
-  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/binary.h \
-  global.h mcp2515.h mcp2515_defs.h defaults.h
